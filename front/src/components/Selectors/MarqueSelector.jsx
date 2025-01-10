eacc7c0101c58de08d87f7da266f6c85627d8057
CHELABI Maroua Cerine <149478955+ChelabiMaroua@users.noreply.github.com>
2025-01-10 14:47:23 +0100

    test14

---
+++ front/src/components/Selectors/MarqueSelector.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import ThemeSelector from './ThemeSelector'; 
+import { MenuItem } from '@mui/material';
+
+const MarqueSelector = () => {
+  const [selectedMarque, setSelectedMarque] = useState('');
+
+  const handleChange = (event) => {
+    setSelectedMarque(event.target.value);
+  };
+
+  return (
+    <ThemeSelector label="Marque du véhicule" value={selectedMarque} onChange={handleChange}>
+      <MenuItem value="Toyota">Toyota</MenuItem>
+      <MenuItem value="Honda">Honda</MenuItem>
+    </ThemeSelector>
+  );
+};
+
+export default MarqueSelector;
